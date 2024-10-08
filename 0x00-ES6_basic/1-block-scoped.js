e3ca986c82b04b90d30bc05608164581c38fa60b
mars <adenijifara@gmail.com>
2024-10-08 23:21:12 +0300

    Task complete

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    var tasknow = true;
+    var task2now = false;
+  }
+
+  return [task, task2];
+}
