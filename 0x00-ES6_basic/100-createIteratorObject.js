726e7cdf97a3fd7cd0fa6f3d20007ccbc4b46e12
mars <adenijifara@gmail.com>
2024-10-13 00:10:14 +0300

    Fix eslint errors

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
   let repoArray = [];
 
   const departments = Object.keys(report.allEmployees);
-  departments.forEach(department => {
+  departments.forEach((department) => {
     repoArray = [...repoArray, ...report.allEmployees[department]];
   });
 
