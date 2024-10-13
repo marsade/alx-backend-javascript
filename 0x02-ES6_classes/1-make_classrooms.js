d2bf7ee43bf79decd80a506b89cd9bbdd27c4029
mars <adenijifara@gmail.com>
2024-10-13 22:04:29 +0300

    Task complete

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeClassrooms() {
+  const classrooms = [];
+
+  classrooms.push(new ClassRoom(19));
+  classrooms.push(new ClassRoom(20));
+  classrooms.push(new ClassRoom(34));
+  return classrooms;
+}
