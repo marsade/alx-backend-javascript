d54a5331b1faf9d57637bfb25a6e2b7042d82162
mars <adenijifara@gmail.com>
2024-10-13 17:41:19 +0300

    Update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,16 @@
 /* eslint-disable */
-import { createUser, uploadPhoto } from "./utils";
+import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup(){
   let promise1 = uploadPhoto();
   let promise2 = createUser();
 
   Promise.all([promise1, promise2]).then((values) => {
-    const ele = Object.values(values[1]);
+    const users = Object.values(values[1]);
+    const body = Object.values(values[0]);
+    const ele = [body[1], ...users];
     console.log(ele.join(' '));
-  })
+  }).catch((error) => {
+    console.log('Signup system offline');
+  });
 }
