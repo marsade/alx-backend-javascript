c5d9f2824b05ba53d62ac52ec29d25057b449a37
mars <adenijifara@gmail.com>
2024-10-13 18:10:58 +0300

    Add return value

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 import { uploadPhoto, createUser } from "./utils";
 
-export default function handleProfileSignup(){
+export default function handleProfileSignup() {
   let promise1 = uploadPhoto();
   let promise2 = createUser();
 
-  Promise.all([promise1, promise2]).then((values) => {
+  return Promise.all([promise1, promise2]).then((values) => {
     const users = Object.values(values[1]);
     const body = Object.values(values[0]);
     const ele = [body[1], ...users];
