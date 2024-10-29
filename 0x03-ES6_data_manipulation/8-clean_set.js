f448a20cb899386a77f4886b8e0140aadbd939fe
mars <adenijifara@gmail.com>
2024-10-29 14:50:23 +0200

    Add modifications

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
   const arr = [];
   set.forEach((element) => {
-    if (element.includes(startString)) {
+    if (typeof element === 'string' && element.includes(startString)) {
       arr.push(element.substring(startString.length, element.length));
     }
   });
