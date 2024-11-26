3201fab5ce4427fe90f89930b0be18cb08bb2eeb
mars <adenijifara@gmail.com>
2024-11-26 20:53:54 +0200

    Refactor code

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,26 +1,30 @@
 const fs = require('node:fs');
 function countStudents (path) {
-  fs.readFile(path, 'utf8', (error, data) => {
-    if (error) {
-      throw new Error('Cannot load the database');
-    }
+  const csStudents = [];
+  const sweStudents = [];
 
+  try {
+    const data = fs.readFileSync(path, 'utf8');
     const students = data.split('\n').slice(1);
     const count = students.length;
-    const csStudents = [];
-    const sweStudents = [];
-
-    for (const index of students) {
-      if (index.includes('CS')) {
-        csStudents.push(index.split(',')[0]);
-      } else if (index.includes('SWE')) {
-        sweStudents.push(index.split(',')[0]);
+    
+      for (const index of students) {
+        if (index.includes('CS')) {
+          csStudents.push(index.split(',')[0]);
+        } else if (index.includes('SWE')) {
+          sweStudents.push(index.split(',')[0]);
+        }
       }
-    }
-    console.log(`Number of students: ${count}`);
-    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  });
+
+      console.log(`Number of students: ${count}`);
+      console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+      console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+    } catch (error) {
+    throw Error('Cannot load the database')
+  }
 }
 
+
+// Example usage
+countStudents('database.csv');
 module.exports = countStudents;
