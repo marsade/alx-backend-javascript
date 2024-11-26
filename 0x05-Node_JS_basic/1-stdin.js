2b02b63eef4256104bcd280c254cebd34e16d17b
mars <adenijifara@gmail.com>
2024-11-26 12:17:09 +0200

    Fix lint errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,8 +5,7 @@ const readline = require('readline').createInterface({
 
 readline.question('Welcome to Holberton School, what is your name?\n', name => {
   console.log(`Your name is: ${name}`);
-  readline.on('close', ()=> {
+  readline.on('close', () => {
     console.log('This important software is now closing');
-  })
+  });
 });
-
