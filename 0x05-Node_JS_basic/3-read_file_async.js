5375bea12acde835a1d7d6e0a5a752fb0d46d143
Mars Adeniji <adenijifara@gmail.com>
2024-11-30 16:24:54 +0200

    Task 3 complete

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,19 +1,32 @@
-const fs = require('fs/promises');
+const { readFile } = require('fs');
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const students = data.split('\n').slice(1);
-    const count = students.length - 1;
-    const csStudents = students.filter(student => student.includes('CS'));
-    const sweStudents = students.filter(student => student.includes('SWE'));
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    try {
+      readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          reject(Error('Cannot load the database'));
+        } else {
+          const students = data.split('\n').slice(1);
+          const count = students.length - 1;
+          const csStudents = students.filter(student => student.includes('CS')).map(
+            student => student.split(',')[0]
+          );
+          const sweStudents = students.filter(student => student.includes('SWE')).map(
+            student => student.split(',')[0]
+          );
 
-    console.log(`Number of students: ${count}`);
-    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+          console.log(`Number of students: ${count}`);
+          console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+          console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+
+          resolve(data);
+        }
+      });
+    } catch (error) {
+      reject(Error('Cannot load the database'));
+    }
+  });
 }
 
 module.exports = countStudents;
